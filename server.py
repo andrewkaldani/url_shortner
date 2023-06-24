df6d05dc71cf90cf9e0415a0f8b35700f2ee7534
andrewkaldani <akaldani@scu.edu>
2023-06-24 12:02:32 -0700

    using response object

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify, redirect, Response
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import hashlib
 import base64
 import random
 import os
+import json
 app = Flask(__name__)
 
 load_dotenv()
@@ -33,9 +34,8 @@ def shortner(long_url):
     return final_url[:7]
 
 def error_message(message):
-    res = jsonify({'message':message})
-    res.status_code = 400
-    return res
+    res = json.dumps({'message':message})
+    return Response(res, status=400, mimetype='application/json')
 
 @app.route("/", methods = ["GET"])
 def challenge():
@@ -73,21 +73,22 @@ def add_url():
         new_url = Url(key=key, short_url=shorten_url, long_url= long_url)
         db.session.add(new_url)
         db.session.commit()
-        res = jsonify({
+        res = json.dumps({
             "key": key,
             "long_url": long_url,
             "short_url": shorten_url
         })
-        res.status_code = 302
-        return res
+        return Response(res, status=302, mimetype='application/json')
+
     else:
-        res = jsonify({
+        res = json.dumps({
             "msg":"url already exsists",
             "key":url_db.key,
             "short_url":url_db.short_url,
             "long_url": url_db.long_url
         })
-        return res
+        return Response(res, status=302, mimetype='application/json')
+
 
 @app.route("/redirect/<key>", methods = ["GET"])
 def redirect_url(key):
@@ -109,15 +110,15 @@ def delete_url():
     url = request.json['url']
     check = Url.query.filter((Url.key == url)|(Url.long_url==url)).first()
     if check is None:
-        res = jsonify( "This url does not exist there is nothing to delete")
-        res.status_code = 404
-        return res
+        res = json.dumps( "This url does not exist there is nothing to delete")
+        return Response(res, status=404, mimetype='application/json')
+
     else:
         db.session.delete(check)
         db.session.commit()
-        res = jsonify( "Successfully Deleted the url from the database")
-        res.status_code = 202
-        return res
+        res = json.dumps( "Successfully Deleted the url from the database")
+        return Response(res, status=202, mimetype='application/json')
+
 
 
 if __name__ == '__main__':
