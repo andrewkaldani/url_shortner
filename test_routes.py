ad41e0244bf0c48f2fb48061f40560124e5805d3
andrewkaldani <akaldani@scu.edu>
2023-06-24 21:48:57 -0700

    ...

--- test_routes.py
+++ test_routes.py
@@ -3,7 +3,7 @@ import json
 import pytest
 import coverage
 import requests
-import requests_mock as rm_module
+import requests_mock
 from server import app, shortner, Url, db
 app.app_context().push()
 db.create_all()
@@ -23,7 +23,17 @@ def test_database_url():
     assert test_url.short_url == "https://localhost:wrtzhyr"
     assert test_url.long_url == "https://gmail.com"
 
+def test_query_db():
+    test_url = Url("KtM5543","https://localhost/KtM5543","https://gmail.com")
+    db.session.add(test_url)
+    url_db = Url.query.filter(Url.long_url == "https://gmail.com").first()
+    assert url_db.key == "KtM5543"
+    assert url_db.short_url == "https://localhost/KtM5543"
+    assert url_db.long_url == "https://gmail.com"
 
+    url_db = Url.query.filter(Url.long_url == "https://shouldbenone.com").first()
+    assert url_db == None
+    
 def test_shortner():
     urla = "https://google.com"
     urlb = "https://codingchallenges.fyi/challenges/challenge-url-shortener"
@@ -95,16 +105,6 @@ def test_url_key():
         assert test == False
 
 
-def test_query_db():
-    test_url = Url("KtM5543","https://localhost/KtM5543","https://gmail.com")
-    db.session.add(test_url)
-    url_db = Url.query.filter(Url.long_url == "https://gmail.com").first()
-    assert url_db.key == "KtM5543"
-    assert url_db.short_url == "https://localhost/KtM5543"
-    assert url_db.long_url == "https://gmail.com"
-
-    url_db = Url.query.filter(Url.long_url == "https://shouldbenone.com").first()
-    assert url_db == None
 
 def test_url_redirect():
     client = app.test_client()
